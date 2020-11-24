09c3e8d3233c62928fbe4eaf740acdc12fd7a4b2
Gytis Žaltauskas <gytis.zaltauskas@gmail.com>
2020-11-24 12:06:37 +0200

    Pridėta žaidimo laimėjimo ir pralaimėjimo logika

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const numbers = document.querySelector(".numbers");
+const messageElement = document.querySelector(".message");
 
 function generateButtons() {
+    numbers.innerHTML = "";
+
     for (let i = 0; i < 10; i++) {
         let randomNumber = Math.floor((Math.random() * 100) + 1);
         numbers.innerHTML += `<button onclick="onButtonClick(this)">${randomNumber}</button>`;
@@ -11,9 +14,27 @@ generateButtons();
 
 function onButtonClick(button) {
     const currentNumber = Number(button.innerHTML);
-    for (let number of numbers.children){
-        if (currentNumber < Number(number.innerHTML))
+    for (let number of numbers.children) {
+        if (currentNumber < Number(number.innerHTML)) {
+            console.log("Radome didesnį skaičių");
+            setMessage("Jūs pralaimėjote! Žaiskite iš naujo!");
+            generateButtons();
             return;
+        }
     }
+
     button.remove();
+
+    if (!numbers.children.length) {
+        setMessage("Jūs laimėjote! Žaiskite iš naujo!");
+        generateButtons();
+    }
+}
+
+function setMessage(message) {
+    messageElement.innerHTML = message;
+
+    setTimeout(() => {
+        messageElement.innerHTML = "";
+    }, 3000);
 }
