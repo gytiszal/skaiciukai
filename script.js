4066b97e6ebfb7b0c10f38e1ddf05b7d56fd4cba
Gytis Žaltauskas <gytis.zaltauskas@gmail.com>
2020-11-24 10:55:08 +0200

    Suformatuotas kodas

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function generateButtons() {
     const numbers = document.querySelector(".numbers");
+    
     for (let i = 0; i < 10; i++) {
         let randomNumber = Math.floor((Math.random() * 100) + 1);
         numbers.innerHTML += `<button>${randomNumber}</button>`;
     }
 }
+
 generateButtons();
 
