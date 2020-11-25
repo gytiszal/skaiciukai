cd83a885f67dbcd9a826624809f08eb95f666cd6
D-Pakamore <D-Pakamore@users.noreply.github.com>
2020-11-25 10:27:25 +0200

    skaiciukai in progress

--- script.js
+++ script.js
@@ -1,40 +1,127 @@
-const numbers = document.querySelector(".numbers");
-const messageElement = document.querySelector(".message");
 
-function generateButtons() {
-    numbers.innerHTML = "";
+const UI = {
+    numbers: document.querySelector(".numbers"),
+    messageElement: document.querySelector(".message"),
+    startButton: document.querySelector(".startButton"),
+    gameOptions: document.querySelector(".gameOptions"),
+    displayHours: document.querySelector("#displayHours"),
+    displayMinutes: document.querySelector("#displayMinutes"),
+    displaySeconds: document.querySelector("#displaySeconds")
 
-    for (let i = 0; i < 10; i++) {
+};
+
+
+function generateButtonsEasy() {
+    UI.numbers.innerHTML = "";
+    UI.gameOptions.style.display = "none";
+    
+    
+    
+
+    for (let i = 0; i < 4; i++) {
         let randomNumber = Math.floor((Math.random() * 100) + 1);
-        numbers.innerHTML += `<button onclick="onButtonClick(this)">${randomNumber}</button>`;
+        UI.numbers.innerHTML += `<button onclick="onButtonClick(this)">${randomNumber}</button>`;
     }
 }
 
-generateButtons();
+function generateButtonsMedium() {
+    UI.numbers.innerHTML = "";
+    UI.gameOptions.style.display = "none";
+
+    for (let i = 0; i < 10; i++) {
+        let randomNumber = Math.floor((Math.random() * 100) + 1);
+        UI.numbers.innerHTML += `<button onclick="onButtonClick(this)">${randomNumber}</button>`;
+    }
+}
+
+function generateButtonsHard() {
+    UI.numbers.innerHTML = "";
+    UI.gameOptions.style.display = "none";
+
+    for (let i = 0; i < 22; i++) {
+        let randomNumber = Math.floor((Math.random() * 100) + 1);
+        UI.numbers.innerHTML += `<button onclick="onButtonClick(this)">${randomNumber}</button>`;
+    }
+}
+
 
 function onButtonClick(button) {
-    const currentNumber = Number(button.innerHTML);
-    for (let number of numbers.children) {
+ 
+  
+  
+     
+   
+  const currentNumber = Number(button.innerHTML);
+    for (let number of UI.numbers.children) {
         if (currentNumber < Number(number.innerHTML)) {
-            console.log("Radome didesnį skaičių");
             setMessage("Jūs pralaimėjote! Žaiskite iš naujo!");
-            generateButtons();
+            callStartMenu();
+            UI.numbers.innerHTML = "";
             return;
         }
     }
 
     button.remove();
 
-    if (!numbers.children.length) {
+    
+
+    if (!UI.numbers.children.length) {
+        
         setMessage("Jūs laimėjote! Žaiskite iš naujo!");
-        generateButtons();
+        callStartMenu();
     }
 }
 
 function setMessage(message) {
-    messageElement.innerHTML = message;
+    UI.messageElement.innerHTML = message;
 
     setTimeout(() => {
-        messageElement.innerHTML = "";
+        UI.messageElement.innerHTML = "";
     }, 3000);
 }
+
+function choseYourDifficulty() {
+    UI.gameOptions.innerHTML = `
+    <div onclick="generateButtonsEasy()">Easy</div>
+    <div onclick="generateButtonsMedium()">Medium</div>
+    <div onclick="generateButtonsHard()">I love pain</div>`;
+}
+
+function callStartMenu() {
+UI.gameOptions.innerHTML = `
+<div onclick="choseYourDifficulty()" class="startButton">Start</div>`;
+UI.gameOptions.style.display = "flex";
+}
+
+
+function timeToString(time) {
+    let diffInHrs = time / 3600000;
+    let hh = Math.floor(diffInHrs);
+  
+    let diffInMin = (diffInHrs - hh) * 60;
+    let mm = Math.floor(diffInMin);
+  
+    let diffInSec = (diffInMin - mm) * 60;
+    let ss = Math.floor(diffInSec);
+   
+    let fixedHh = hh - 446170;
+
+    let formattedHH = fixedHh.toString().padStart(2, "0");
+    let formattedMM = mm.toString().padStart(2, "0");
+    let formattedSS = ss.toString().padStart(2, "0");
+
+    // setInterval(function(){ 
+    //     UI.displayHours.innerHTML = `${formattedHH}`;
+    //     UI.displayMinutes.innerHTML = `${formattedMM}`;
+    //     UI.displaySeconds.innerHTML = `${formattedSS}`;
+    //  }, 1000);
+    
+  
+    return `${formattedHH}:${formattedMM}:${formattedSS}`;
+    
+}
+
+// timeToString(Date.now());
+  
+
+
